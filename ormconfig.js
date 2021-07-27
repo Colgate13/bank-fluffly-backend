f1f2a99bd614924a51c8cde2597c6dc06e26bb76
Colgate13 <gabreilbarros13@gmail.com>
2021-07-27 09:11:35 -0300

    Modific ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,10 +1,10 @@
 export default {
-  type: process.env.TYPEDB,
-  host: process.env.HOSTDB,
-  port: process.env.PORTDB,
-  username: process.env.USERNAME,
-  password: process.env.PASS,
-  database: process.env.DB,
+  type: process.env.TYPEORM_CONNECTION,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
   entities: [
     './src/models/*.ts',
   ],
