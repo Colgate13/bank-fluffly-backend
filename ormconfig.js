373f81ad77939458a30c94c8e0bc4df51a023f3f
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2021-08-16 11:13:31 -0300

    Update ormconfig.js

--- ormconfig.js
+++ ormconfig.js
@@ -1,3 +1,8 @@
+console.log(`process.env.DATABASE = ${process.env.DATABASE}`;
+console.log(`process.env.ENTITIES = ${process.env.ENTITIES}`;
+console.log(`process.env.MIGRATIONS = ${process.env.MIGRATIONS}`;
+console.log(`process.env.CLIMIGRATIONSDIR = ${process.env.CLIMIGRATIONSDIR}`;
+
 module.exports = {
   "type": "sqlite",
   "database": process.env.DATABASE,
