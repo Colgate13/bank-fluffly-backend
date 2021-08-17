aa586a156a73926169b4ad062ac91cd2df8668b5
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2021-08-17 10:10:13 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -25,6 +25,14 @@ app.use((err: Error, request: Request, response: Response, _next: NextFunction) 
       message: err.message,
     });
   }
+  
+  
+  // Se a mensagem não for conhecida, ou seja, não usamos o AppError, ele retorna Erro internal
+  // statusCode 500
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
 });
 
 app.listen(PORT, () => console.log(`🐱‍👤> Server running! 🧛‍♀️ ${PORT}`));
